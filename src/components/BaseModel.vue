24f21d3a165b8d292af826023f0313a92dac3508
neptoo <heywuq@outlook.com>
2026-07-09 18:20:31 +0800

    弹窗开发

--- src/components/BaseModel.vue
+++ src/components/BaseModel.vue
@@ -1,13 +1,20 @@
 <template>
-    <div v-show="modalActive" class="absolute w-full bg-black-30 h-screen top-0 left-0 flex justify-center px-8">
-        <div v-if="modalActive" class="p-6 bg-white self-start mt-32 max-screen-md rounded-md">
-            <slot></slot>
-            <button class="text-white bg-primary py-2 px-6 mt-8">关闭</button>
-        </div>
-    </div>
+    <Teleport to="body">
+        <Transition name="outer">
+            <div v-show="modalActive" class="absolute w-full bg-black/30 h-screen top-0 left-0 flex justify-center px-8">
+                <Transition name="inner">
+                    <div v-if="modalActive" class="p-6 bg-white self-start mt-32 max-screen-md rounded-md">
+                        <slot></slot>
+                        <button class="text-white bg-primary py-2 px-6 mt-8 cursor-pointer" @click="$emit('close-modal')">关闭</button>
+                    </div>
+                </Transition>
+            </div>
+        </Transition>
+    </Teleport>
 </template>
 
 <script setup>
+defineEmits(['close-modal'])
 defineProps({
     modalActive: {
         type: Boolean,
@@ -17,6 +24,31 @@ defineProps({
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.outer-enter-active,
+.outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.outer-enter-from,
+.outer-leave-to {
+  opacity: 0;
+}
+
+.inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.inner-leave-to {
+  transform: scale(0.8);
+}
 
 </style>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,50 +1,55 @@
 <template>
-    <header class="sticky top-0 bg-primary shadow-lg">
-        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
-            <RouterLink :to="{ name: 'home' }">
-                <div class="flex items-center gap-3">
-                    <i class="fa-solid fa-sun text-2xl"></i>
-                    <p class="text-2xl">当地天气</p>
-                </div>
-            </RouterLink>
-            <div class="flex gap-3 flex-1 justify-end">
-                <i class="fa-solid fa-circle-info text-xl hover:text-secondary duration-150 cursor-pointer"></i>
-                <i class="fa-solid fa-plus text-xl hover:text-secondary duration-150 cursor-pointer"></i>
-            </div>
-            <BaseModel>
-                <div class="text-black">
-                    <h1 class="text-2xl mb-1">关于本站：</h1>
-                    <p class="mb-4">
-                        可查看所选城市的当前天气和未来天气
-                    </p>
-                    <h2 class="text-2xl">使用方法：</h2>
-                    <ol class="list-decimal list-inside mb-4">
-                        <li>
-                            在搜索框中输入城市名称，即可搜索对应的城市
-                        </li>
-                        <li>
-                            在搜索结果中选择一个城市，可以直接打开该城市的天气页面
-                        </li>
-                        <li>
-                            点击右上角的“+”图标来添加和保存城市，之后可以在主页中查看
-                        </li>
-                    </ol>
+  <header class="sticky top-0 bg-primary shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">当地天气</p>
+        </div>
+      </RouterLink>
+      <div class="flex gap-3 flex-1 justify-end">
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-secondary duration-150 cursor-pointer"
+          @click="toggleModal"
+        ></i>
+        <i
+          class="fa-solid fa-plus text-xl hover:text-secondary duration-150 cursor-pointer"
+        ></i>
+      </div>
+      <BaseModel :modalActive="modalActive" @close-modal="toggleModal">
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">关于本站：</h1>
+          <p class="mb-4">可查看所选城市的当前天气和未来天气</p>
+          <h2 class="text-2xl">使用方法：</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>在搜索框中输入城市名称，即可搜索对应的城市</li>
+            <li>在搜索结果中选择一个城市，可以直接打开该城市的天气页面</li>
+            <li>
+              点击右上角的“+”图标来添加和保存城市，之后可以在主页中查看该城市
+            </li>
+          </ol>
 
-                    <h2 class="text-2xl">移除城市</h2>
-                    <p>
-                        如果你不再需要关注某个城市，请在主页中选中该城市。然后在页面底部会有删除该城市的选项
-                    </p>
-                </div>
-
-            </BaseModel>
-        </nav>
-    </header>
+          <h2 class="text-2xl">移除城市</h2>
+          <p>
+            如果你不再需要关注某个城市，请在主页中选中该城市，然后在页面底部会有删除该城市的选项
+          </p>
+        </div>
+      </BaseModel>
+    </nav>
+  </header>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
-import BaseModel from './BaseModel.vue';
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+import BaseModel from "./BaseModel.vue";
 
+const modalActive = ref(false);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
 
 <style lang="scss" scoped></style>
