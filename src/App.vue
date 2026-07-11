d913a1451a305217eba3cc9993eeabc21e99395c
neptoo <heywuq@outlook.com>
2026-07-11 21:36:19 +0800

    搜索

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import NavigationBar from './components/NavigationBar.vue';
+import HomeView from './views/HomeView.vue';
 
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen bg-primary">
     <NavigationBar />
+    <HomeView />
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
-    <div>
-
-    </div>
+    <main class="container text-white">
+        <div class="pt-4 mb-8 relative">
+            <input type="text" v-model="searchKeyword" class="w-full py-2 px-1 bg-transparent border-b border-gray-400
+            focus:outline-0 focus:border-secondary focus:shadow-lg" placeholder="搜索">
+        </div>
+    </main>
 </template>
 
-<script>
-export default {
-    setup () {
-        
+<script setup>
+import { ref } from 'vue';
 
-        return {}
-    }
-}
+const searchKeyword = ref("")
 </script>
 
 <style lang="scss" scoped>
